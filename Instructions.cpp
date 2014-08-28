9a5362e4f3323ab5445dc83276c59237933813e8
Justin Madru <justin.jdm64@gmail.com>
2014-08-27 23:48:19 -0700

    casting to pointer should always be an error
    using Inst::CastTo() to cast a value to a pointer should result in
    an error. pointer math (++/--) is already handled by skipping CastTo()

--- Instructions.cpp
+++ Instructions.cpp
@@ -48,10 +48,7 @@ void Inst::CastTo(RValue& value, SType* type, CodeContext& context)
 		context.addError("can not cast complex types");
 		return;
 	} else if (type->isPointer()) {
-		if (!valueType->isInteger()) {
-			context.addError("operation with pointer only valid with integer");
-			return;
-		}
+		context.addError("can't cast value to pointer type");
 		return;
 	} else if (type->isVec()) {
 		if (valueType->isNumeric()) {
