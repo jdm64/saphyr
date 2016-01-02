f6a07716836aee74a85ba91d28953421cb85641b
Justin Madru <justin.jdm64@gmail.com>
2016-01-01 16:31:43 -0800

    add LLVM_VER to cpp2llvm.sh to use non default llvm tooling

--- scripts/cpp2llvm.sh
+++ scripts/cpp2llvm.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+# export LLVM_VER to use a non default llvm version
+
 file=$(basename "$1")
 fileext=${file##*.}
 filename=${file%.*}
 
 if [ $fileext == "c" ]; then
-	clang -S -emit-llvm "$1"
+	clang$LLVM_VER -S -emit-llvm "$1"
 else
-	clang++ -std=c++11 -S -emit-llvm "$1"
+	clang++$LLVM_VER -std=c++11 -S -emit-llvm "$1"
 fi
 
-llc -march=cpp "$filename".ll -o "$filename".ll.cpp
+llc$LLVM_VER -march=cpp "$filename".ll -o "$filename".ll.cpp
