1f1187e24fad4ca33371ed28b32f3a4acad384a2
Justin Madru <justin.jdm64@gmail.com>
2016-10-22 15:18:56 -0700

    remove llvm 3.7 from the travis build
    llvm 3.7 was removed from apt.llvm.org as they only host the last
    two stable releases. A new source for 3.7 could be found later.

--- travisSetup.sh
+++ travisSetup.sh
@@ -10,9 +10,6 @@ sudo sed -i "s/precise/trusty/g" /etc/apt/sources.list.d/trusty.list
 if [[ $LLVM_VER == "-3.8" ]]; then
 	echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main" | sudo tee /etc/apt/sources.list.d/llvm38.list > /dev/null
 	echo "UNITTEST_ARG = +-3.8" > src/Configfile
-elif [[ $LLVM_VER == "-3.7" ]]; then
-	echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main" | sudo tee /etc/apt/sources.list.d/llvm37.list > /dev/null
-	echo "UNITTEST_ARG = +-3.7" > src/Configfile
 fi
 
 sudo apt-get update -qq
