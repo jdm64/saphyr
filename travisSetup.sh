53b21511a017b569ebf56863e1934a1e77118f55
Justin Madru <justin.jdm64@gmail.com>
2017-03-20 23:44:59 -0700

    switch to trusty (14.04) travis image

--- travisSetup.sh
+++ travisSetup.sh
@@ -4,9 +4,6 @@ if [[ $CXX == "g++" ]]; then
 	CXX_PKG="g++"
 fi
 
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.d/trusty.list
-sudo sed -i "s/precise/trusty/g" /etc/apt/sources.list.d/trusty.list
-
 if [[ $LLVM_VER == "-3.8" ]]; then
 	echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main" | sudo tee /etc/apt/sources.list.d/llvm38.list > /dev/null
 	echo "UNITTEST_ARG = +-3.8" > src/Configfile
