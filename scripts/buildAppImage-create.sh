fde6e9c0b7353f53d1b04f8353fdad0203f2093c
Justin Madru <justin.jdm64@gmail.com>
2017-08-11 00:49:13 -0700

    update AppImage to v9

--- scripts/buildAppImage-create.sh
+++ scripts/buildAppImage-create.sh
@@ -2,8 +2,8 @@
 
 AppImageToolBin=$(mktemp)
 
-wget https://github.com/probonopd/AppImageKit/releases/download/8/AppRun-x86_64 -O AppImage/AppRun
-wget https://github.com/probonopd/AppImageKit/releases/download/8/appimagetool-x86_64.AppImage -O $AppImageToolBin
+wget https://github.com/AppImage/AppImageKit/releases/download/9/AppRun-x86_64 -O AppImage/AppRun
+wget https://github.com/AppImage/AppImageKit/releases/download/9/appimagetool-x86_64.AppImage -O $AppImageToolBin
 chmod +x AppImage/AppRun
 chmod +x $AppImageToolBin
 
