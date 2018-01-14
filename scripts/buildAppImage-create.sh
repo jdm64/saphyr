ae5bb66c3274e87e197408db9acaa6bfc117bc31
Justin Madru <justin.jdm64@gmail.com>
2018-01-13 16:56:25 -0800

    update AppImage to v10

--- scripts/buildAppImage-create.sh
+++ scripts/buildAppImage-create.sh
@@ -2,8 +2,8 @@
 
 AppImageToolBin=$(mktemp)
 
-wget https://github.com/AppImage/AppImageKit/releases/download/9/AppRun-x86_64 -O AppImage/AppRun
-wget https://github.com/AppImage/AppImageKit/releases/download/9/appimagetool-x86_64.AppImage -O $AppImageToolBin
+wget https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64 -O AppImage/AppRun
+wget https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage -O $AppImageToolBin
 chmod +x AppImage/AppRun
 chmod +x $AppImageToolBin
 
