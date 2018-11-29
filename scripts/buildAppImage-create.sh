9aaacb16abf470d39e7ca0aa5ad33948dd668e7d
Justin Madru <justin.jdm64@gmail.com>
2018-11-28 21:56:06 -0800

    update AppImage version to 11

--- scripts/buildAppImage-create.sh
+++ scripts/buildAppImage-create.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 AppImageToolBin=$(mktemp)
+AppImageVer=11
 
-wget https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64 -O AppImage/AppRun
-wget https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage -O $AppImageToolBin
+wget https://github.com/AppImage/AppImageKit/releases/download/$AppImageVer/AppRun-x86_64 -O AppImage/AppRun
+wget https://github.com/AppImage/AppImageKit/releases/download/$AppImageVer/appimagetool-x86_64.AppImage -O $AppImageToolBin
 chmod +x AppImage/AppRun
 chmod +x $AppImageToolBin
 
