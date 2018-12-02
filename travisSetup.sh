5f772af260db0e1d4ae6a60f2f0da18bf6945453
Justin Madru <justin.jdm64@gmail.com>
2018-12-01 22:25:16 -0800

    only call apt-get update once in travisSetup.sh

--- travisSetup.sh
+++ travisSetup.sh
@@ -10,19 +10,18 @@ echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-6.0 main" | sudo tee
 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 
+OTHER_PKGS=""
+
 if [[ $LLVM_VER == "-3.8" ]] || [[ $LLVM_VER == "-4.0" ]]; then
 	VER=${LLVM_VER:1}
 	echo "UNITTEST_ARG = +$VER-5.0" > src/Configfile
-	sudo apt-get update -qq
-	sudo apt-get install -qq -y llvm-5.0-dev
+	OTHER_PKGS="llvm-5.0-dev"
 elif [[ $LLVM_VER == "-6.0" ]]; then
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-	sudo apt-get update -qq
-else
-	sudo apt-get update -qq
 fi
 
-sudo apt-get install -qq -y flexc++ bisonc++ llvm$LLVM_VER-dev $CXX_PKG libboost-program-options-dev libboost-filesystem-dev python3 binutils
+sudo apt-get update -qq
+sudo apt-get install -qq -y flexc++ bisonc++ llvm$LLVM_VER-dev $CXX_PKG libboost-program-options-dev libboost-filesystem-dev python3 binutils $OTHER_PKGS
 
 if [[ $COVERAGE == "true" ]]; then
 	sudo pip install -U cpp-coveralls
