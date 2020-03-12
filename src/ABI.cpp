ee298ca0bb980c2d608dc5623f0d37870b84f3be
Justin Madru <justin.jdm64@gmail.com>
2020-03-12 00:29:35 -0700

    more updates

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -38,14 +38,37 @@ llvm_abi::Type convert(SType* ty)
 {
 	if (ty->isInteger()) {
 		if (ty->isBool())
-			return llvm_abi::BoolTy;
+			return BoolTy;
+
+		auto isUnsigned = ty->isUnsigned();
 		switch (ty->size()) {
 		case 8:
-			return llvm_abi::Int8Ty;
+			return isUnsigned ? UInt8Ty : Int8Ty;
+		case 16:
+			return isUnsigned ? UInt16Ty : Int16Ty;
+		case 32:
+			return isUnsigned ? UInt32Ty : Int32Ty;
+		case 64:
+			return isUnsigned ? UInt64Ty : Int64Ty;
 		}
+	} else if (ty->isFloating()) {
+		return ty->isDouble() ? DoubleTy : FloatTy;
+	} else if (ty->isPointer()) {
+		return PointerTy;
+	} else if (ty->isVoid()) {
+		return VoidTy;
+	} else if (ty->isStruct()) {
+		SStructType* sTy = static_cast<SStructType*>(ty);
+
+		vector<llvm_abi::Type> types;
+		for (auto it = sTy->begin(); it != sTy->end(); it++) {
+
+		}
+		TypeBuilder builder;
+		return builder.getStructTy(types);
 	}
 	
-	return llvm_abi::BoolTy;
+	return BoolTy;
 }
 
 RValue SABI::CreateCall(CodeContext& context)
